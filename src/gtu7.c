00e5a37775e75d4949604339a5daa5bde18787cc
Weinkyls <miaoxn.chong@gmail.com>
2026-03-12 00:17:50 +0000

    integrated gps implementation
    is currently compiling and printing to serial, but i've been testing it in my room so the gps data is garbage. will test outside tommorrow

--- src/gtu7.c
+++ src/gtu7.c
@@ -40,7 +40,7 @@ bool gps_init(uart_inst_t *uart, uint8_t tx, uint8_t rx, uint16_t baud) {
     return uart_status;
 }
 
-bool read_uart(char *line, int max_length){
+bool read_gtu7_uart(char *line, int max_length){
     static uint16_t index = 0;
 
     if (uart_is_readable(GTU7_UART)){
