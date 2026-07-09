e01be413278d81431546645d8269a23558c94937
Eric Dawe <eric@redocrehtona.dev>
2026-07-09 13:36:06 +0100

    Changed order and added explicit gpio_init calls to sensor i2c initialisation

--- src/sensor_i2c.c
+++ src/sensor_i2c.c
@@ -7,9 +7,11 @@
 
 
 void sensor_i2c_init(void) {
-    i2c_init(SENSOR_I2C, 100 * 1000);
+    gpio_init(SENSOR_SDA);
+    gpio_init(SENSOR_SCL);
     gpio_set_function(SENSOR_SDA, GPIO_FUNC_I2C);
     gpio_set_function(SENSOR_SCL, GPIO_FUNC_I2C);
+    i2c_init(SENSOR_I2C, 100 * 1000);
     // gpio_pull_up(SENSOR_SDA);
     // gpio_pull_up(SENSOR_SCL);
 }
